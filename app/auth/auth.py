015d3e45be5c8a6f6397d08ba3f7ed06d38b15a2
YuraIvasiutyn <yuraivasyutin221@gmail.com>
2025-05-27 13:19:27 +0200

    dz-13

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from typing import Optional
 
+import redis.asyncio as redis
 from fastapi import Depends, HTTPException
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
@@ -11,14 +12,16 @@ from starlette import status
 from app.database.db import get_db
 from app.models.db_models import User
 from app.models.user_models import UserModel
+from app.conf.config import settings
 
 
 class Hash:
     pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
-    SECRET_KEY = "secret_key"
-    ALGORITHM = "HS256"
+    SECRET_KEY = settings.secret_key
+    ALGORITHM = settings.algorithm
 
     oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+    r = redis.Redis(host=settings.redis_host, port=settings.redis_port, db=0)
 
     def verify_password(self, plain_password, hashed_password):
         return self.pwd_context.verify(plain_password, hashed_password)
@@ -83,12 +86,29 @@ class Hash:
             raise credentials_exception
         return user
 
+    def create_email_token(self, data: dict):
+        to_encode = data.copy()
+        expire = datetime.utcnow() + timedelta(days=7)
+        to_encode.update({"iat": datetime.utcnow(), "exp": expire})
+        token = jwt.encode(to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM)
+        return token
+
+    async def get_email_from_token(self, token: str):
+        try:
+            payload = jwt.decode(token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
+            email = payload["sub"]
+            return email
+        except JWTError as e:
+            print(e)
+            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                                detail="Invalid token for email verification")
+
 
 async def get_user_by_email(email: str, db: Session) -> User:
     return db.query(User).filter(User.username == email).first()
 
 
-async def create_user(self, body: UserModel, db: Session) -> User:
+async def create_user(body: UserModel, db: Session) -> User:
     new_user = User(**body.dict())
     db.add(new_user)
     db.commit()
--- app/database/db.py
+++ app/database/db.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from app.conf.config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:567234@localhost:5431/rest_app"
+
+SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
